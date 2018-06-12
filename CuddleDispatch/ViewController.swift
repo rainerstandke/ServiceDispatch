e2f0fe98697b35f79056e2f39ad0fb2b34de4e01
Rainer Standke <rainer@xmil.biz>
2018-06-12 10:46:01 -0700

    Initial Commit

---
+++ CuddleDispatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CuddleDispatch
+//
+//  Created by Rainer Standke on 6/12/18.
+//  Copyright © 2018 Rainer Standke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
